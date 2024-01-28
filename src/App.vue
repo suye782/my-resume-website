bb85df430f3bc91c1bb41caf283c0e4a1ae3fa10
suye782 <suye782@gmail.com>
2024-01-28 17:32:02 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <header class="header scroll-header">
+    <AppNav></AppNav>
+  </header>
+  <main class="main">
+    <AppHome></AppHome>
+    <AppAbout></AppAbout>
+    <AppQualification></AppQualification>
+    <AppSkills></AppSkills>
+    <AppPortfolio></AppPortfolio>
+    <AppContact></AppContact>
+  </main>
+  <AppFooter></AppFooter>
+</template>
+
+<script>
+import AppNav from './components/AppNav.vue'
+import AppHome from './components/AppHome.vue';
+import AppAbout from './components/AppAbout.vue';
+import AppSkills from './components/AppSkills.vue';
+import AppQualification from './components/AppQualification.vue';
+import AppPortfolio from './components/AppPortfolio.vue';
+import AppContact from './components/AppContact.vue';
+import AppFooter from './components/AppFooter.vue'
+export default {
+  name: 'App',
+  components: {
+    AppNav,
+    AppHome,
+    AppAbout,
+    AppSkills,
+    AppQualification,
+    AppPortfolio,
+    AppContact,
+    AppFooter
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/styles/index.scss";
+</style>
